ae047295b1e4ade50e902a7c459e65ecbe50a5d5
James Blades <jwkblades+github@gmail.com>
2016-02-05 13:37:52 -0700

    Made branch more descriptive for git repositories
    Branch will now display the first 7 characters of a hash if we aren't on a
    branch, as well as show you if you have changes awaiting pull or push.

--- src/StandardSuite/Branch.sh
+++ src/StandardSuite/Branch.sh
@@ -1,6 +1,11 @@
 BRANCH_DIRTY_FG=160
 BRANCH_CLEAN_FG=166
 BRANCH_BAR_BG=235
+BRANCH_FADE_FG=240
+
+BRANCH_CHAR="⎇ "
+BRANCH_IN="↓"
+BRANCH_OUT="↑"
 
 determineBranch()
 {
@@ -13,14 +18,35 @@ determineBranch()
             repo=""
         fi
     fi
+    local incoming=0
+    local outgoing=0
+
+    local inColor=${BRANCH_FADE_FG}
+    local outColor=${BRANCH_FADE_FG}
+
     if [[ -n "${branch}" ]]; then
         local dirty=""
         if [[ ${repo} == "hg" ]]; then
             dirty="$($(return $(hg status 2>/dev/null | wc -l)) && echo '${BRANCH_CLEAN_FG}' || echo '${BRANCH_DIRTY_FG};1')"
         elif [[ ${repo} == "git" ]]; then
-            dirty="$($(git diff-files --quiet &>/dev/null) && echo '${BRANCH_CLEAN_FG}' || echo '${BRANCH_DIRTY_FG};1')"
+            if [[ "${branch}" == "HEAD" ]]; then
+                branch="$(git rev-parse --short HEAD 2>/dev/null)"
+            fi
+            dirty="$($(return $(git status --porcelain 2>/dev/null | wc -l)) && echo '${BRANCH_CLEAN_FG}' || echo '${BRANCH_DIRTY_FG};1')"
+            local remote="$(git rev-parse --symbolic-full-name --abbrev-ref @{upstream} 2>/dev/null)"
+            if [[ -n "${remote}" ]]; then
+                local deltas="$(git log --format='%m' --topo-order --left-right ${branch}...${remote} 2>/dev/null)"
+                outgoing=$(echo "${deltas}" | grep "^<" | wc -l)
+                incoming=$(echo "${deltas}" | grep "^>" | wc -l)
+                if [[ ${incoming} -gt 0 ]]; then
+                    inColor=${BRANCH_DIRTY_FG}
+                fi
+                if [[ ${outgoing} -gt 0 ]]; then
+                    outColor=${BRANCH_DIRTY_FG}
+                fi
+            fi
         fi
-        branch=" \[\033[38;5;${dirty}m\]⎇ \[\033[38;5;${BRANCH_CLEAN_FG}m\]${branch} "
+        branch=" \[\033[38;5;${dirty}m\]${BRANCH_CHAR}\[\033[38;5;${BRANCH_CLEAN_FG}m\]${branch} \[\033[$(bartlet_color 1 ${inColor})m\]${BRANCH_IN}\[\033[$(bartlet_color 1 ${outColor})m\]${BRANCH_OUT} "
     fi
     echo -n "${branch}"
 }
