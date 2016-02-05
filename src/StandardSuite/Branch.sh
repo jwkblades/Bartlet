75d0b50318661c4182ddddd273b6bfd69b3b619b
James Blades <jwkblades+github@gmail.com>
2016-02-05 16:10:49 -0700

    Added extended branch functionality for hg too
    Along with an option to disable it (since it is significantly slower than
    git's). It is disabled by default too.

--- src/StandardSuite/Branch.sh
+++ src/StandardSuite/Branch.sh
@@ -3,6 +3,8 @@ BRANCH_CLEAN_FG=166
 BRANCH_BAR_BG=235
 BRANCH_FADE_FG=240
 
+BRANCH_SHOW_HG_STATUS=0
+
 BRANCH_CHAR="⎇ "
 BRANCH_IN="↓"
 BRANCH_OUT="↑"
@@ -28,6 +30,16 @@ determineBranch()
         local dirty=""
         if [[ ${repo} == "hg" ]]; then
             dirty="$($(return $(hg status 2>/dev/null | wc -l)) && echo '${BRANCH_CLEAN_FG}' || echo '${BRANCH_DIRTY_FG};1')"
+            if [[ ${BRANCH_SHOW_HG_STATUS} -eq 1 ]]; then
+                $(hg in -l 1 &>/dev/null)
+                if [[ $? -eq 0 ]]; then
+                    inColor=${BRANCH_DIRTY_FG}
+                fi
+                $(hg out -l 1 &>/dev/null)
+                if [[ $? -eq 0 ]]; then
+                    outColor=${BRANCH_DIRTY_FG}
+                fi
+            fi
         elif [[ ${repo} == "git" ]]; then
             if [[ "${branch}" == "HEAD" ]]; then
                 branch="$(git rev-parse --short HEAD 2>/dev/null)"
